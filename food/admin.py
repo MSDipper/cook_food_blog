9934c581842c584514056e3dd71956d50761541b
MSDipper <github5248@gmail.com>
2022-09-25 23:29:08 +0300

    added models and plugin ckeditor, initial setting conection food in setting.py

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from foods.models import Category, Tag, Food, Comment
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',), }
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',), }
+
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',), }
+    save_on_top = True
+    save_as = True
+    
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- food/models.py
+++ food/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+from ckeditor.fields import RichTextField
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=150, unique=True, verbose_name='URL')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=150, unique=True, verbose_name='URL')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+class Food(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Title')
+    image = models.ImageField(upload_to='images/', verbose_name='Image')
+    description = RichTextField(verbose_name='Text')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Date_create')
+    slug = models.SlugField(max_length=250, unique=True, verbose_name='URL')
+    category = models.ForeignKey(
+        Category, 
+        related_name='food',
+        verbose_name='category',
+        on_delete=models.SET_NULL, 
+        null=True,
+        blank=True
+        )
+    tag = models.ManyToManyField(Tag, verbose_name='Tags')
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Food'
+        verbose_name_plural = 'Foods'
+
+class Comment(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=150)
+    email = models.EmailField(max_length=254)
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Date')
+    photo = models.ImageField(verbose_name='Photo', upload_to='image/', blank=True, null=True)
+    message = models.TextField(max_length=500, verbose_name='Text')
+    post = models.ForeignKey(Food, related_name='comment', verbose_name='Comment', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
+    
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
